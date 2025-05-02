1870a8389534d4419786d3c510b6cd30d5e68c17
bhanuprasad911 <bhanuprasadchintu.170403@gmail.com>
2025-05-02 15:12:35 +0530

    socket issue

--- src/context/BotContext.jsx
+++ src/context/BotContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://192.168.0.15:8001");
+const socket = io("https://finaleval-backend-1.onrender.com/");
 
 export const BotCOntext = createContext();
 
